eefcae2e89a55259fb1afe64117008d037f0677a
Gokay BURUC <gokayburuc@yandex.com>
2023-03-18 12:49:27 +0300

    scraper updated

--- koeri-scraper.py
+++ koeri-scraper.py
@@ -13,7 +13,7 @@ url = 'http://www.koeri.boun.edu.tr/scripts/lst1.asp'
 USER_AGENT = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0"}
 
-# get content
+# GET CONTENT
 
 
 def GetContent(url):
